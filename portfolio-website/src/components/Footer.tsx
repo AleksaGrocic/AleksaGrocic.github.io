9200e431b5ee9714ed25e367b6d43bd98c8c8cd4
AleksaGrocic <agrocic@gmail.com>
2026-01-31 13:00:05 +0100

    Adding contact info

--- portfolio-website/src/components/Footer.tsx
+++ portfolio-website/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="footer">
-      <p className="footerText">© 2025 Aleksa Gročić</p>
+      <p className="footerText">© 2026 Aleksa Gročić</p>
     </div>
   );
 }
--- portfolio-website/src/pages/Contact.tsx
+++ portfolio-website/src/pages/Contact.tsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 
+interface ContactData {
+  email: string;
+  linkedin: string;
+  instagram: any[];
+  cv: string;
+}
+
 export default function Contact() {
-  const [data, setData] = useState<{ page: string } | null>(null);
+  const [data, setData] = useState<ContactData | null>(null);
 
   useEffect(() => {
     fetch("/content/contact.json")
@@ -13,7 +20,17 @@ export default function Contact() {
 
   return (
     <div className="bodyContainer">
-      <p>{data.page}</p>
+      <div className="socialMediaContainer">
+      {data.instagram.map((instagram, i) => (
+        <pre key={i}>
+          <p>{instagram.type}</p>
+          <img src="/assets/instagram.svg" alt="Instagram icon"/>
+          <span className="coloredWord"><a href={instagram.link} target="_blank" rel="noreferrer">
+          {instagram.name}
+          </a></span>
+        </pre>
+      ))}
+      </div>
     </div>
   );
 }
