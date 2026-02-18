307aa5a99ec895d546af7c7765ec2585f9a4419e
Aleksa Gročić <agrocic@gmail.com>
2026-02-18 16:23:33 +0100

    Updated Contact tab

--- portfolio-website/src/pages/Contact.tsx
+++ portfolio-website/src/pages/Contact.tsx
@@ -4,6 +4,7 @@ interface ContactData {
   email: string;
   linkedin: string;
   instagram: any[];
+  github: string;
   cv: string;
 }
 
@@ -21,15 +22,18 @@ export default function Contact() {
   return (
     <div className="bodyContainer">
       <div className="socialMediaContainer">
-      {data.instagram.map((instagram, i) => (
-        <pre key={i}>
-          <p>{instagram.type}</p>
-          <img src="/assets/instagram.svg" alt="Instagram icon"/>
-          <span className="coloredWord"><a href={instagram.link} target="_blank" rel="noreferrer">
-          {instagram.name}
-          </a></span>
-        </pre>
-      ))}
+        <a href={data.github} target="_blank" rel="noreferrer"><img src="/assets/github.png" alt="Github icon"/><span className="coloredWord"> Github</span></a>
+        <a href={data.linkedin} target="_blank" rel="noreferrer"><img src="/assets/linkedin.png" alt="LinkedIn icon"/><span className="coloredWord"> LinkedIn</span></a>
+        <a href={data.cv} target="_blank" rel="noreferrer"><img src="/assets/cv.png" alt="CV icon"/><span className="coloredWord"> My CV</span></a>
+        <p>Instagram</p>
+        {data.instagram.map((instagram, i) => (
+          <pre key={i}>
+            <a href={instagram.link} target="_blank" rel="noreferrer">
+              <img src="/assets/instagram.svg" alt="Instagram icon"/>
+              <span className="coloredWord"> {instagram.name}</span> <span className="grayWord">({instagram.type})</span>
+            </a>
+          </pre>
+        ))}
       </div>
     </div>
   );
